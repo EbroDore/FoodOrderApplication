d32fd5acce79a1a3968bf501d361b796263fd2c4
EbroDore <brodybond13@gmail.com>
2023-03-20 12:13:02 +1100

    Initial Commit1

---
+++ ResturantAPI/Models/OrderMaster.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ResturantAPI.Models
+{
+    public class OrderMaster
+    {
+        public long OrderMasterId { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public string? OrderNumber { get; set; }
+
+        public int CustomerId { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string? PMethod { get; set; }
+
+        public decimal GTotal { get; set; }
+        
+
+        //navigational property
+        public Customer? Customer { get; set; }
+    }
+}
